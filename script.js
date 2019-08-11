c1d45a64a23c8432d69e306d30cf210b35bd19da
Semen <chynenovsemen@gmail.com>
2019-08-11 03:58:30 +0300

    Added floating labels and styles

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-function disableSubmit() {
-    document.getElementById("submit").disabled = true;
-   }
+"use strict"
+
+function Float() {
+
+  const FloatLabel = (() => {
+
+      // add active class and placeholder
+      const handleFocus = (e) => {
+        const target = e.target;
+        target.parentNode.classList.add('active');
+        target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+      };
+      
+      // remove active class and placeholder
+      const handleBlur = (e) => {
+        const target = e.target;
+        if(!target.value) {
+          target.parentNode.classList.remove('active');
+        }
+        target.removeAttribute('placeholder');
+      };
+
+      // register events
+      const bindEvents = (element) => {
+        const floatField = element.querySelector('input');
+        floatField.addEventListener('focus', handleFocus);
+        floatField.addEventListener('blur', handleBlur);
+      };
+      
+      // get DOM elements
+      const init = () => {
+        const floatContainers = document.querySelectorAll('.inputWrapper');
+
+        floatContainers.forEach((element) => {
+          if (element.querySelector('input').value) {
+              element.classList.add('active');
+          }
+          
+          bindEvents(element);
+        });
+      };
+
+      return {
+        init: init
+      };
+    })();
   
-function activateButton(element) {
+    FloatLabel.init();
   
-    if(element.checked) {
-        document.getElementById("submit").disabled = false;
-    } else {
-        document.getElementById("submit").disabled = true;
-    }
-  
-}
+  }
